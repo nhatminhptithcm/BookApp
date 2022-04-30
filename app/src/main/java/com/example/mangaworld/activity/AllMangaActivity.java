207938f98b4142ca677ace1619299c5e2a05b3b0
nhatminhptithcm <nhatminhptithcm@gmail.com>
2022-04-30 16:48:00 +0700

    Init

---
+++ app/src/main/java/com/example/mangaworld/activity/AllMangaActivity.java
@@ -0,0 +1,17 @@
+package com.example.mangaworld.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mangaworld.R;
+
+public class AllMangaActivity extends AppCompatActivity {
+
+    //Comment 0430 AllMangaActivity
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_manga);
+    }
+}
